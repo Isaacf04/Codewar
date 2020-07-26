f70b89793be220c26b63a0970e3dcc5352395ea2
Isaacf <isaacf04@gmail.com>
2020-07-26 03:11:30 +0000

    Two problems

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 
+
+const arr = ['keep', 'remove', 'keep', 'remove'];
 function removeEveryOther(arr){
+  
   return arr.filter((_, index) => index % 2 === 0);
+ 
 }
 
 
-let arr = ['keep', 'remove', 'keep', 'remove'];
-
 console.log(arr);
+
+function count(string) {
+const count = {};
+string.split(' ').forEach(char => {
+  count[char] = count[char] ? (count[char]++) : 1;
+});
+
+}
